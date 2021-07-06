eaa2bd3e9814531d54d8e1b98d0894950ec8563a
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-07 01:45:54 +0200

    chessboard is on screen

--- gui/chess_board_gui.cpp
+++ gui/chess_board_gui.cpp
@@ -2,29 +2,96 @@
 // Created by studio25 on 06.07.2021
 //
 #include "chess_board_gui.h"
+
 void ChessBoardGui::ClearHighlight() {
   highlighted_squares_.clear();
   highlighted_pieces_.clear();
 }
 ChessBoardGui::ChessBoardGui() : local_board_() {
 
-  window_ = SDL_CreateWindow("chess",           // window title
-                            100,              // initial x position
-                            100,              // initial y_ position
-                            9 * 64,           // width, in pixels
-                            8 * 64,           // height, in pixels
-                            SDL_WINDOW_OPENGL // flags - see below
+  current_orientation_ = WHITE_UP;
+
+  window_ = SDL_CreateWindow("chess",          // window title
+                             100,              // initial x position
+                             100,              // initial y_ position
+                             9 * 64,           // width, in pixels
+                             8 * 64,           // height, in pixels
+                             SDL_WINDOW_OPENGL // flags - see below
   );
 
   renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_PRESENTVSYNC);
+
+  DrawSquares();
+
+  window_thread_ = new std::thread(&ChessBoardGui::ThEventLoop, this);
 }
-void ChessBoardGui::UpdateScreen() {
-  SDL_RenderPresent(renderer_);
+void ChessBoardGui::UpdateScreen() { SDL_RenderPresent(renderer_); }
+
+void ChessBoardGui::UpdateDisplay(ChessBoard &board) {
+  DrawSquares();
+  local_board_ = board;
 }
-void ChessBoardGui::UpdateDisplay(ChessBoard &board) { local_board_ = board; }
 
 ChessBoardGui::~ChessBoardGui() {
+
+  window_thread_->join();
   SDL_DestroyRenderer(renderer_);
   SDL_DestroyWindow(window_);
-  //SDL_Quit();
+
+  // SDL_Quit();
+}
+void ChessBoardGui::ThEventLoop() {
+
+  while (1 < 2) {
+
+    if (SDL_PollEvent(&event_)) {
+
+      switch (event_.type) {
+      case SDL_QUIT:
+        return;
+      case SDL_MOUSEMOTION:
+
+        break;
+      case SDL_MOUSEBUTTONUP:
+
+        break;
+      }
+    }
+    // std::this_thread::sleep_for(std::chrono::milliseconds(24));
+  }
+}
+void ChessBoardGui::DrawSquares() {
+
+  for (int y = 0; y < 8; y++)
+    for (int x = 0; x < 8; x++) {
+      SDL_Rect square = {y * 64, x * 64, 64, 64};
+
+      if (current_orientation_ == WHITE_UP and (x % 2 == 0 xor y % 2 == 0))
+        SDL_SetRenderDrawColor(renderer_, LIGHT_WHITE_COLOR);
+      else
+        SDL_SetRenderDrawColor(renderer_, LIGHT_BLACK_COLOR);
+
+      SDL_RenderFillRect(renderer_, &square);
+    }
+
+  for (auto &c : highlighted_squares_) {
+
+    SDL_Rect square = {c.first / 8, c.first % 8, 64, 64};
+    switch (c.second) {
+    case WHITE:
+      SDL_SetRenderDrawColor(renderer_, WHITE_COLOR);
+      break;
+    case BLACK:
+      SDL_SetRenderDrawColor(renderer_, BLACK_COLOR);
+      break;
+    case BLUE:
+      SDL_SetRenderDrawColor(renderer_, BLUE_COLOR);
+      break;
+    case RED:
+      SDL_SetRenderDrawColor(renderer_, RED_COLOR);
+      break;
+    }
+    SDL_SetRenderDrawColor(renderer_, 255, 255, 255, 0);
+    SDL_RenderFillRect(renderer_, &square);
+  }
 }
--- gui/chess_board_gui.h
+++ gui/chess_board_gui.h
@@ -14,6 +14,28 @@
 #include "move.h"
 
 #include <vector>
+#include <chrono>
+#include <thread>
+
+#define WHITE_COLOR 255,255,255,0
+#define LIGHT_WHITE_COLOR 200,200,200,0
+#define BLACK_COLOR 0,0,0,0
+#define LIGHT_BLACK_COLOR 74,74,74,0
+#define BLUE_COLOR 0,0,255,0
+#define RED_COLOR 255,0,0,0
+enum GuiColor{
+  WHITE,
+  BLACK,
+  BLUE,
+  RED
+
+};
+enum Orientation{
+  WHITE_UP,
+  BLACK_UP
+
+
+};
 
 class ChessBoardGui {
 public:
@@ -27,18 +49,26 @@ public:
 
   void AnimateMove(Move animate);
 
+  void DrawSquares();
+
   void ClearHighlight();
+
+  void ThEventLoop();
 
   ~ChessBoardGui();
 
 protected:
   ChessBoard local_board_;
-  std::vector<std::pair<unsigned, SDL_Color>> highlighted_squares_;
-  std::vector<std::pair<unsigned, SDL_Color>> highlighted_pieces_;
+  std::vector<std::pair<int, GuiColor>> highlighted_squares_;
+  std::vector<std::pair<int, GuiColor>> highlighted_pieces_;
 
   SDL_Event event_;
   SDL_Window *window_;
   SDL_Renderer *renderer_;
+
+  std::thread* window_thread_;
+
+  Orientation  current_orientation_;
 };
 
 #endif // CONSOLE_CHESS_GUI_CHESS_BOARD_GUI_H_
