cbcb791312aeb6d8c80a1251df48da2f271bdce9
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-07 13:12:59 +0200

    the cmake with SDL is fuck

--- gui/button.cpp
+++ gui/button.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "button.h"
+void Button::DrawToRenderer(SDL_Renderer *renderer) {
+
+  SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image_);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_DestroyTexture(texture);
+
+}
--- gui/button.h
+++ gui/button.h
@@ -5,6 +5,23 @@
 #ifndef CONSOLE_CHESS_GUI_BUTTON_H_
 #define CONSOLE_CHESS_GUI_BUTTON_H_
 
-class Button {};
+#include <SDL_rect.h>
+#include <SDL_surface.h>
+#include <SDL_render.h>
+
+
+class Button {
+
+  SDL_Rect placement_;
+  SDL_Surface* image_ = SDL_LoadBMP("image.bmp");
+
+
+  void DrawToRenderer(SDL_Renderer *renderer);
+~Button(){
+
+
+  SDL_FreeSurface(image_);
+}
+};
 
 #endif // CONSOLE_CHESS_GUI_BUTTON_H_
