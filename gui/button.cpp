14b29d9ebc2a07bd067f11562108bb771af547ea
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-07 11:10:56 +0200

    new class button

---
+++ gui/button.cpp
@@ -0,0 +1,5 @@
+//
+// Created by studio25 on 07.07.2021.
+//
+
+#include "button.h"
---
+++ gui/button.h
@@ -0,0 +1,10 @@
+//
+// Created by studio25 on 07.07.2021.
+//
+
+#ifndef CONSOLE_CHESS_GUI_BUTTON_H_
+#define CONSOLE_CHESS_GUI_BUTTON_H_
+
+class Button {};
+
+#endif // CONSOLE_CHESS_GUI_BUTTON_H_
