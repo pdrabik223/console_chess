f6dec040bf56e3c4bdeeed0398c3e49f61eedde2
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-24 16:04:35 +0200

    the functions require so many little things, I need to make my chessboard more colorful

--- gui/admin_gui.cpp
+++ gui/admin_gui.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "admin_gui.h"
+AdminGui::AdminGui() {}
+Move AdminGui::MinMaxAll(int depth, bool color, int threads) {
+  auto t_1 = std::chrono::steady_clock::now();
+  std::vector<Move> move_buffer;
+  game_.GenAllPossibleMoves(color, move_buffer);
+
+  DisplayMoves(move_buffer, color);
+
+  for (auto &i : move_buffer) {
+    ChessBoard i_board(game_);
+    game_.TransposeChessboard(i_board, i);
+    i.evaluation_ = game_.MinMax(i_board, depth, color);
+  }
+
+  double elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+                            std::chrono::steady_clock::now() - t_1)
+                            .count();
+
+  std::cout << "elapsed time: ";
+  std::cout << std::to_string(elapsed_time) << " ms\n";
+
+  DisplayBestMoves(move_buffer, color); // he sorts move_buffer
+  return move_buffer.front();
+}
+void AdminGui::DisplayBestMoves(std::vector<Move> &move_buffer) {
+  for (auto i : move_buffer) {
+    gui_handle_.HighlightMove(i);
+  }
+}
--- gui/admin_gui.h
+++ gui/admin_gui.h
@@ -5,13 +5,41 @@
 #ifndef CONSOLE_CHESS_GUI_ADMIN_GUI_H_
 #define CONSOLE_CHESS_GUI_ADMIN_GUI_H_
 
-#include "chess_board_gui.h"
 #include "chess_board.h"
+#include "chess_board_gui.h"
+#include <windows_console/full_command.h>
 
 class AdminGui {
 
+public:
+  AdminGui();
+
+
+
+private:
+  Move MinMaxAll(int depth, bool color, int threads);
+  void MinMax(int depth, int position, int threads);
+
+  Move AlfaBetaMinMaxAll(int depth, bool color, int threads);
+  void AlfaBetaMinMax(int depth, int position, int threads);
+
+  Move AlfaBetaMinMaxWTranspositionTableAll(int depth, bool color, int threads);
+  void AlfaBetaMinMaxWTranspositionTable(int depth, int position, int threads);
+
+  void Help();
+  void ShowPossible();
+  void ShowPossible(int position);
+  void AddPiece(full_command input);
+  void DisplayMoves( std::vector<Move> &move_buffer, bool color);
+  void DisplayBestMoves(std::vector<Move> &move_buffer);
+
+  void MakeEmFight(std::vector<int> settings);
+
+  void Load();
+
   ChessBoard game_;
-  ChessBoardGui console_handle_;
+  ChessBoardGui gui_handle_;
+
 };
 
 #endif // CONSOLE_CHESS_GUI_ADMIN_GUI_H_
